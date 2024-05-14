e5814a3c6f3f36e0053416d4a49ff51755d0aa22
SamuelDesN <vazquezsamuel877@gmail.com>
2024-05-14 19:59:44 +0200

    modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+let usuarios=['Gonzalo','Pedro','Luis','Gonzalo',]
+let apellido=['Fernandez','Gomez','Poca','Silva',]
+let img=['./imagenes/user.png','./imagenes/user.png','./imagenes/user.png','./imagenes/user.png']
+let desc=['Estudiante en la asiociacion','Profesor en la asociacion','Estudiante en la asociacion','Director de la asociacion']
+$(function(){
+    for(let i=0;i<usuarios.length;i++){
+        var crearusuario=$('<div class="carta"> <img src="'+img[i]+'"> <p class="nombre">'+ usuarios[i]+'</p> <p class="apellido">'+apellido[i]+'</p> <p class="desc">'+desc[i]+'</p> </div>')
+        $('#personas').append(crearusuario);}
+    }) 
+
 $(function(){
     $('#boton').click(function(){
         var resultado = $('#buscarPersona').val().toLowerCase();
